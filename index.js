c93d723cda87d67686dab0c45a002e89bec15276
BERTRAND ZANCO <bertrand.zanco@adeo.com>
2020-01-23 09:52:41 +0100

    Test

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
- core.debug(`CI tools actioins`);
+    core.debug(`CI tools actioins`);
+    console.log(__dirname)
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
